b39dd7c386bbd00b75f4b2f9693e3cffd243827c
Mat Cook <mat.cook@bigpond.com>
2020-04-23 15:57:24 +0800

    fixed article template to only query 1 article and render on refresh

--- src/components/ArticleTemplate/ArticleTemplate.js
+++ src/components/ArticleTemplate/ArticleTemplate.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useQuery } from "@apollo/react-hooks";
 import { gql } from "apollo-boost";
 import styles from "./ArticleTemplate.module.scss";
 
 const news = gql`
-  {
-    articles(sort: "Date:asc") {
+  query news($slug: String!) {
+    articles(sort: "Date:asc", where: { Slug: $slug }) {
       id
       Title
       Date
@@ -20,18 +20,7 @@ const news = gql`
 `;
 
 const ArticleTemplate = (props) => {
-  const [article, setArticle] = useState({
-    id: "",
-    Title: "",
-    Date: "",
-    Body: "",
-    Slug: "",
-    Image: {
-      url: "",
-      name: "",
-    },
-  });
-
+  const slug = props.match.params.slug;
   const days = [
     "Sunday",
     "Monday",
@@ -56,48 +45,47 @@ const ArticleTemplate = (props) => {
     "December",
   ];
 
-  const { loading, error, data } = useQuery(news);
-  const fetchData = async () => {
-    const slug = props.match.params.slug;
+  const { loading, error, data } = useQuery(news, {
+    variables: {
+      slug,
+    },
+  });
 
-    if (loading) {
-      return <span>Loading...</span>;
-    }
-    if (error) {
-      console.error(error);
-    }
-    if (data) {
-      const article = data.articles.find((article) => article.Slug === slug);
-      setArticle(article);
-    }
-  };
+  if (loading) {
+    return (
+      <div className="container">
+        <span>Loading...</span>
+      </div>
+    );
+  }
+  if (error) {
+    console.error(error);
+  }
+  if (data) {
+    const article = data.articles[0];
 
-  useEffect(() => {
-    fetchData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    const articleDate = new Date(article.Date);
 
-  const articleDate = new Date(article.Date);
-
-  const day = days[articleDate.getDay()];
-  const month = months[articleDate.getMonth()];
-  const year = articleDate.getFullYear();
-  return (
-    <div className="container">
-      <img
-        className={styles.articleImage}
-        src={`https://api.gnfl.com.au${article.Image.url}`}
-        alt={article.Image.name}
-      />
-      <h3 className={styles.heading}>{article.Title}</h3>
-      <span className={styles.date}>
-        {article.Date.length > 0
-          ? `${day} ${month} ${articleDate.getDate()}, ${year}`
-          : null}
-      </span>
-      <p className={styles.body}>{article.Body}</p>
-    </div>
-  );
+    const day = days[articleDate.getDay()];
+    const month = months[articleDate.getMonth()];
+    const year = articleDate.getFullYear();
+    return (
+      <div className="container">
+        <img
+          className={styles.articleImage}
+          src={`https://api.gnfl.com.au${article.Image.url}`}
+          alt={article.Image.name}
+        />
+        <h3 className={styles.heading}>{article.Title}</h3>
+        <span className={styles.date}>
+          {article.Date.length > 0
+            ? `${day} ${month} ${articleDate.getDate()}, ${year}`
+            : null}
+        </span>
+        <p className={styles.body}>{article.Body}</p>
+      </div>
+    );
+  }
 };
 
 export default ArticleTemplate;
